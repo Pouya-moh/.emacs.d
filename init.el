8536acf7c66a1c2e6940fedc91e3454ac0dc4c55
pouya-moh <other.pouya@gmail.com>
2019-01-03 15:25:11 +0100

    add the basics

---
+++ init.el
@@ -0,0 +1,37 @@
+
+;; package manager and related
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
+;(package-initialize)
+(unless package--initialized (package-initialize t))
+
+;; (thanks to https://github.com/daedreth)
+;; Bootstrapping use-package 
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+(package-install 'use-package))
+
+;;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
+(when (file-readable-p "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("a8ca72df3fc0b021dbc61a29bdc8bdf15e8335dd658bef98ae43b89cb8648d21" default))
+ '(markdown-command "/usr/local/bin/markdown")
+ '(package-selected-packages '(try use-package magit markdown-mode))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "nil" :family "DejaVuSansMono Nerd Font")))))
