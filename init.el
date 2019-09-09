3bca4e76250f4edf679f6f2a103e6636b7a5e65c
pouya-moh <other.pouya@gmail.com>
2019-09-09 14:21:30 +0200

    commiting some changes

--- init.el
+++ init.el
@@ -22,3 +22,4 @@
 ;;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
 (when (file-readable-p "~/.emacs.d/config.org")
   (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+
